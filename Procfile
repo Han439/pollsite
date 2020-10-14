d7873476e252e781739c49fb0513eb102c9cf75f
Han439 <hanafi4399@gmail.com>
2020-10-15 01:13:18 +0700

    modified Profile 2

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn poll.wsgi
+web: gunicorn pollsite.wsgi
--- pollsite/settings.py
+++ pollsite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'bpoiaocp1)7+aiafk6nrlm%!8jz#k-k3rn93bkh+a0%m#b5t%='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['pollsiteapp.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
